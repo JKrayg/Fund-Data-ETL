f63a3346b1bb6f9c521c0f4f025886d012962446
jkrayg <jk1019602@gmail.com>
2026-01-23 00:09:07 -0500

    insert/tables-from-metadata

--- db/schema.py
+++ db/schema.py
@@ -5,48 +5,64 @@ from db.db_connection import connect
 
 connection = connect()
 cursor = connection.cursor()
-Tables = {}
+tables = {}
 
 def tables_from_metadata(metadata):
-    urls = {'FUND_REPORTED_HOLDING.tsv':'fact_holdings',
-            'IDENTIFIERS.tsv':'dim_assets',
-            'FUND_REPORTED_INFO.tsv':'dim_funds'}
+    urls = {'IDENTIFIERS.tsv':'dim_assets',
+            'FUND_REPORTED_INFO.tsv':'dim_funds',
+            'FUND_REPORTED_HOLDING.tsv':'fact_holdings'}
     
-    tablez = {}
+    schemas = {}
+
+    # metadata = metadata[::-1]
 
     for i in metadata:
         url = i['url']
         columns = i['tableSchema']['columns']
         c = ' '
+        # build schema
         for j in columns:
             if j['datatype']['base'] == 'string':
                 j['datatype']['base'] = 'VARCHAR'
-                q = j['name'] + ' ' + j['datatype']['base'] + "(" + str(j['datatype']['maxLength']) + ')'
-                if 'required' in j:
+                q = j['name'].lower() + ' ' + j['datatype']['base'] + "(" + str(j['datatype']['maxLength']) + ')'
+                if 'required':
                     q += ' NOT NULL'
                 q += ', '
                 c += q
             else:
                 j['datatype']['base'] = 'NUMERIC'
-                q = j['name'] + ' ' + j['datatype']['base'] + "(" + \
+                q = j['name'].lower() + ' ' + j['datatype']['base'] + "(" + \
                     str(j['datatype']['dataPrecision']) + ',' + \
                     str(j['datatype']['dataScale']) + ')'
-                if 'required' in j:
+                if 'required':
                     q += ' NOT NULL'
                 q += ', '
                 c += q
 
         c += 'PRIMARY KEY (' + ', '.join(i['tableSchema']['PrimaryKey']) + ')'
+        if url == 'FUND_REPORTED_HOLDING.tsv':
+            c += (", FOREIGN KEY (ACCESSION_NUMBER) REFERENCES dim_funds(ACCESSION_NUMBER)"
+                  ", FOREIGN KEY (HOLDING_ID, IDENTIFIERS_ID) REFERENCES dim_assets(HOLDING_ID, IDENTIFIERS_ID)")
         
-        tablez[urls[url]] = c
+        schemas[urls[url]] = c
 
+    # print(json.dumps(tables, indent=4))
     for u in urls.keys():
-        Tables[u] = ("CREATE TABLE " + urls[u] + " (" + tablez[urls[u]] + ') ENGINE=InnoDB')
+        tables[u] = ("CREATE TABLE " + urls[u] + " (" + schemas[urls[u]] + ") ENGINE=InnoDB")
 
-    for t in Tables:
-        cursor.execute(Tables[t])
+    for t in tables:
+        cursor.execute(tables[t])
     connection.close()
 
+    # return tables
+
+def insert(table_name, columns, values):
+    in_ = ("INSERT INTO " + table_name +
+        " (" + ', '.join(columns) + ") VALUES (" + ', '.join(['%s' for _ in range(len(columns))]) + ")")
+    
+    # print(values)
+    cursor.executemany(in_, values)
+    connection.commit()
 
 def drop_tables():
     cursor.execute("DROP TABLES IF EXISTS "
--- fund_etl.py
+++ fund_etl.py
@@ -4,7 +4,7 @@ import numpy as np
 import uuid
 import json
 from db.db_connection import connect
-from db.schema import tables_from_metadata, drop_tables
+from db.schema import tables_from_metadata, drop_tables, insert
 from etl.clean import clean, fix_exchange_rate, currency_to_usd
 
 
@@ -23,10 +23,6 @@ def tsv_to_df(filename, limit):
             nrows=limit)
     
 
-# connect to sql
-# connection = connect()
-# cursor = connection.cursor()
-
 with open('data/form_nport/nport_metadata.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
@@ -42,91 +38,19 @@ for i in tables:
 
 tables_from_metadata(metadata)
 
-
-# print(json.dumps(metadata[1], indent=4))
-
-    # print(json.dumps(i, indent=4))
-# urls = [i['url'] for i in data['tables']]
-#     print([d for d in data['tables'][0]])
-
 reported_holdings = tsv_to_df('data/form_nport/FUND_REPORTED_HOLDING.tsv', 300000)
 reported_holdings.columns = [c.lower() for c in reported_holdings.columns]
-# reported_holdings = clean(reported_holdings)
-
-# print(json.dumps(reported_holdings.iloc[0:10].to_dict(), indent=4))
-# reported_holdings = reported_holdings.drop(['derivative_cat', 'fair_value_level',
-#                                             'is_restricted_security', 'other_issuer',
-#                                             'other_asset', 'payoff_profile',
-#                                             'issuer_title'], axis=1)
-
 
 
 assets = tsv_to_df('data/form_nport/IDENTIFIERS.tsv', 0)
 assets.columns = [c.lower() for c in assets.columns]
-# assets = clean(assets)
-
-# grab assets present in reported holdings batch
-# assets = assets[assets['asset_key'].isin(reported_holdings['asset_key'])]
 assets = assets[assets['holding_id'].isin(reported_holdings['holding_id'])]
-# assets = assets[['asset_key', 'asset_id', 'asset_symbol']]
-
-# assets = assets.merge(reported_holdings[['holding_id', 'issuer_name',
-#                                          'asset_cat', 'issuer_type',
-#                                          'issuer_lei', 'asset_cusip'
-#                                         ]], on='asset_key', how='left')
-
-# insert assets data into assets table
-# query = ("INSERT INTO dim_assets "
-#         "(asset_key, asset_id, asset_symbol, "
-#         "asset_name, asset_category, asset_type, "
-#         "asset_lei, asset_cusip) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
-# cursor.executemany(query, assets.values.tolist())
-# connection.commit()
+insert('dim_assets', assets.columns, assets.values.tolist())
 
 
 funds = tsv_to_df('data/form_nport/FUND_REPORTED_INFO.tsv', 0)
 funds.columns = [c.lower() for c in funds.columns]
-# funds = clean(funds)
-# funds = funds[funds['fund_key'].isin(reported_holdings['fund_key'])]
 funds = funds[funds['accession_number'].isin(reported_holdings['accession_number'])]
-# funds = funds[['fund_key', 'fund_name', 'fund_id',
-#                        'fund_lei', 'total_assets', 'total_liabilities',
-#                        'net_assets', 'assets_misc_security',
-#                        'assets_invested']]
-
-# insert funds data into funds table
-# funds = funds.drop('accession_number', axis=1)
-# query = ("INSERT INTO dim_funds "
-#         "(fund_key, fund_name, fund_id, fund_lei, total_assets,"
-#         " total_liabilities, net_assets, assets_misc_security, assets_invested)"
-#         " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
-# cursor.executemany(query, funds.values.tolist())
-# connection.commit()
-
-# reported_holdings.insert(2, 'asset_id', assets['asset_id'])
-# reported_holdings = reported_holdings.drop(['asset_name', 'asset_lei',
-#                                             'asset_cusip', 'asset_category',
-#                                             'asset_type'], axis=1)
-
-# reported_holdings = fix_exchange_rate(reported_holdings)
-# reported_holdings = currency_to_usd(reported_holdings)
-# query = ("INSERT INTO fact_holdings "
-#         "(fund_key, asset_key, asset_id, balance, total_assets,"
-#         " total_liabilities, net_assets, assets_misc_security, assets_invested)"
-#         " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)")
-# print(reported_holdings.columns)
-
-
-
-
-
-# connection.close()
-
-
-# invesco_sp_500 = yf.Ticker('SPHQ')
-# print(json.dumps(invesco_sp_500.info, indent=4))
-# sphq_name = invesco_sp_500.info.get('shortName')
-# closing_prices = invesco_sp_500['Close']
 
 
 
